4f53561a7f8c470d2db1f1c6415776a123b4534d
DanilMartemyanov <martidanilka@gmail.com>
2024-11-25 17:59:08 +0300

    feat: initialize FractalImage

--- src/main/java/backend/academy/FractalImage.java
+++ src/main/java/backend/academy/FractalImage.java
@@ -2,4 +2,18 @@ package backend.academy;
 
 public record FractalImage(Pixel[] data, int width, int height) {
 
+    public static FractalImage init(int width, int height) {
+        Pixel[] pixels = new Pixel[width * height];
+        for (int i = 0; i < width * height; i++) {
+            pixels[i] = new Pixel(0, 0, 0, 0);
+        }
+        return new FractalImage(pixels, width, height);
+    }
+
+    public boolean containsPoint(Point point) {
+        return point.x() >= 0 && point.x() < width && point.y() >= 0 && point.y() < height;
+    }
+
+
+
 }
